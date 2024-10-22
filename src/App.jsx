e32308c8271b84c1fb17784ea7b5cd5993d6c27f
giahienpham <giahien14604@gmail.com>
2024-10-22 02:17:55 -0400

    add artcard section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Arts from "./components/Arts";
 
 const App = () => {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
       <Navbar />
+      <Arts/>
     </main>
   );
 };
